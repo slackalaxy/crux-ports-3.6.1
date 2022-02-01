9c89594ca053b2125b3142a488f2c4a9bd840ccb
Petar Petrov <slackalaxy@gmail.com>
2022-02-01 18:13:03 +0200

    deleted:    larebuild/

--- custom/larebuild/laRebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rebuild-lib() {
-    prt-cache update -fr -if -im -is \
-        `for f in $(grep -lr "$*" /usr/lib/ 2> /dev/null | sed 's|.*/||'); \
-        do prt-get fsearch $f | grep '^Found in' | sed -e 's|.*/||' -e 's|:$||'; \
-        done | sort -u | xargs prt-cache isinst | grep -e 'is installed' | awk '{ print $2 }'`; 
-}
-
-rebuild-lib "${1}"
